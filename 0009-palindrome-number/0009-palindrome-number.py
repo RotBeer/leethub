8e3fcd35ede1d9b855cb95cdbc5313f38f238cb0
RotBeer <110042369+RotBeer@users.noreply.github.com>
2023-01-20 09:43:56 +0900

    Time: 49 ms (97.29%), Space: 13.9 MB (12.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        str_x = str(x)
+        for i in range(len(str_x)//2):
+            if str_x[i] != str_x[-(i+1)]:
+                return False
+        return True
